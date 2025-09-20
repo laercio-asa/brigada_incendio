a30324a53579ba1317718897949aa1d29e2cb632
Laercio Azevedo de Sá <93953739+laercio-asa@users.noreply.github.com>
2025-09-19 22:12:07 -0300

    main

--- sete_erros/js/game.js
+++ sete_erros/js/game.js
@@ -1,29 +1,29 @@
-var Game = function(positions){
-	$this 		   = this;
-	$this.vars 	   = { positions: positions, currentPositions: {}, markers: 0, started : false, ended : false };
-	$this.defaults = { radius : 40 };
+var Game = function (positions) {
+	$this = this;
+	$this.vars = { positions: positions, currentPositions: {}, markers: 0, started: false, ended: false };
+	$this.defaults = { radius: 40 };
 	$canvas_main = $("#canvas-main");
 	$canvas = $('.canvas');
 	var positions = positions;
 
 
-	this.init = function(){
+	this.init = function () {
 		$this.createCanvas();
 		$this.bindMouseEvents();
 	}
 
-	this.start = function(){
+	this.start = function () {
 		$this.vars.started = true;
 		$this.vars.ended = false;
 		$this.vars.accepts = 0;
 		$canvas_main.find('.marker').remove();
 		$this.vars.markers = 0;
-		jQuery.extend( $this.vars.currentPositions, $this.vars.positions);
+		jQuery.extend($this.vars.currentPositions, $this.vars.positions);
 	}
 
-	this.createCanvas = function(){
-		$canvas_main.on('mousemove', function(e) {
-			var parentOffset = $(this).parent().offset(); 
+	this.createCanvas = function () {
+		$canvas_main.on('mousemove', function (e) {
+			var parentOffset = $(this).parent().offset();
 			var relX = e.pageX - parentOffset.left;
 			var relY = e.pageY - parentOffset.top;
 
@@ -33,84 +33,84 @@ var Game = function(positions){
 			});
 		});
 
-		$canvas_main.on('mouseenter', function() {
+		$canvas_main.on('mouseenter', function () {
 			$('.cursor').addClass('visible');
 		});
 
-		$canvas_main.on('mouseleave', function() {
+		$canvas_main.on('mouseleave', function () {
 			$('.cursor').removeClass('visible');
 		});
 	}
 
-	this.bindMouseEvents = function() {
-		$canvas_main.on('click', function(event){
-			if( $this.vars.started == true && $this.vars.ended == false )
-			{
+	this.bindMouseEvents = function () {
+		$canvas_main.on('click', function (event) {
+			if ($this.vars.started == true && $this.vars.ended == false) {
 				$this.addMarker(event);
 			}
 		});
 	}
 
-	this.addMarker = function(e){
-		if( $this.vars.markers <= 6 ) {
-			var parentOffset = $(e.target).parent().offset(); 
+	this.addMarker = function (e) {
+		if ($this.vars.markers <= 6) {
+			var parentOffset = $(e.target).parent().offset();
 			var relX = e.pageX - parentOffset.left;
 			var relY = e.pageY - parentOffset.top;
 
-			$canvas_main.append('<div class="marker" data-x="'+relX+'" data-y="'+relY+'"></div>');
+			$canvas_main.append('<div class="marker" data-x="' + relX + '" data-y="' + relY + '"></div>');
 
 			$('.marker:last-child').css({
-				'left': relX-($this.defaults.radius/2),
-				'top': relY-($this.defaults.radius/2)
+				'left': relX - ($this.defaults.radius / 2),
+				'top': relY - ($this.defaults.radius / 2)
 			});
 
 			$this.vars.markers++;
-			if( $this.vars.markers == 7) {
+			if ($this.vars.markers == 7) {
 				$this.vars.ended = true;
 				$this.verify();
 			}
 
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
 
-	this.verify = function(){
+	this.verify = function () {
 		var distancia, $marker, markerPosition;
-		
-		$canvas_main.find('.marker').each(function(i){
-			$marker = $(this);
-			markerPosition = { x : $marker.data('x'), y : $marker.data('y') };
 
-			$.each( $this.vars.currentPositions, function( index, position ) {
-				distancia = Math.sqrt( Math.pow(markerPosition.x - position.x, 2) + Math.pow(markerPosition.y - position.y, 2) );
-				if( distancia < $this.defaults.radius )
-				{
+		$canvas_main.find('.marker').each(function (i) {
+			$marker = $(this);
+			markerPosition = { x: $marker.data('x'), y: $marker.data('y') };
+
+			$.each($this.vars.currentPositions, function (index, position) {
+				distancia = Math.sqrt(Math.pow(markerPosition.x - position.x, 2) + Math.pow(markerPosition.y - position.y, 2));
+				if (distancia < $this.defaults.radius) {
 					$marker.addClass('accept');
 					delete $this.vars.currentPositions[index];
 					$this.vars.accepts++;
 				}
 			});
-					
+
 		});
 
-		alert("Você acertou "+$this.vars.accepts+" de 7 erros.");
+		//alert("Você acertou "+$this.vars.accepts+" de 7 erros.");
+		document.getElementById('acertos').innerText = $this.vars.accepts;
+		const meuModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+		meuModal.show();
 
-		if( $this.vars.accepts == 7 )
-		{
+		if ($this.vars.accepts == 7) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
 
-	this.debug = function(){
-		$.each( $this.vars.positions, function( index, position ) {
+	this.debug = function () {
+		$.each($this.vars.positions, function (index, position) {
 			$canvas_main.append('<div class="target"></div>');
 			$canvas_main.find('.target:last-child').css({
-				'left': (position.x-($this.defaults.radius/2)) + 'px',
-				'top': (position.y-($this.defaults.radius/2)) + 'px'
+				'left': (position.x - ($this.defaults.radius / 2)) + 'px',
+				'top': (position.y - ($this.defaults.radius / 2)) + 'px'
 			});
 		});
 	}
